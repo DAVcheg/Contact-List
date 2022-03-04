3d4508f2011beb346e2a1c3baab615ae1448c46f
Aleksey Dergunov <davcheg@me.com>
2022-03-04 23:46:30 +1000

    Initial Commit

---
+++ Contact List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact List
+//
+//  Created by Aleksey Dergunov on 04.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
